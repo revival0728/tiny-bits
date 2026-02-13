b9df9e11bbb8f4cc6b9d6e9ae584a3ed51dbe899
revival028 <revival0728@gmail.com>
2026-02-13 20:16:43 +0800

    finished analog clock displaying nowtime

--- term-analog-clock/clock.c
+++ term-analog-clock/clock.c
@@ -5,19 +5,20 @@
 #include <assert.h>
 
 #define PI 3.14159265358979323846
-#define MXT 12
+#define CK_T 60
 #define CK_R 50
 #define TTDR 15
 #define MH_R (CK_R * 11 / TTDR)
 #define HH_R (CK_R * 6  / TTDR)
-#define TT_R (CK_R * 3  / TTDR)
+#define HT_R (CK_R * 3  / TTDR)
+#define MT_R (CK_R * 1  / TTDR)
 #define CK_O CK_R
 #define CK_SIZE ((CK_R << 1) | 1)
 #define CK_CH '*'
-#define MH_CH '#'
-#define HH_CH 'O'
+#define MH_CH 'O'
+#define HH_CH '#'
 
-float hand[MXT];
+float hand[CK_T];
 char face[CK_SIZE][CK_SIZE + 1];
 
 void draw(float rad, int sr, int er, char ch) {
@@ -31,27 +32,31 @@ void draw(float rad, int sr, int er, char ch) {
 
 void init() {
     // hand
-    for(int i = 0; i < MXT; ++i) hand[(MXT + 6 - i) % MXT] = 2 * PI * i / MXT;
+    for(int i = 0; i < CK_T; ++i) hand[(CK_T + CK_T / 2 - i) % CK_T] = 2 * PI * i / CK_T;
 
     // face
     for(int i = 0; i < CK_SIZE; ++i) for(int j = 0; j < CK_SIZE; ++j) face[i][j] = ' ';
     for(int i = 0; i < CK_SIZE; ++i) face[i][CK_SIZE] = '\0';
-    for(int i = 0; i < 8 * CK_R; ++i) {
-        draw(2 * PI * i / (8 * CK_R), CK_R, CK_R, CK_CH);
-    }
-    for(int i = 0; i < MXT; ++i) {
-        draw(hand[i], CK_R - TT_R, CK_R, CK_CH);
-    }
+    for(int i = 0; i < 8 * CK_R; ++i) draw(2 * PI * i / (8 * CK_R), CK_R, CK_R, CK_CH);
+    for(int i = 0; i < CK_T; ++i) draw(hand[i], CK_R - MT_R, CK_R, CK_CH);
+    for(int i = 0; i < 12; ++i) draw(2 * PI * i / 12, CK_R - HT_R, CK_R, CK_CH);
     face[CK_O][CK_O] = CK_CH;
 }
-
-void draw_mhand(int t) { draw(hand[t], 1, MH_R, MH_CH); }
-void draw_hhand(int t) { draw(hand[t], 1, HH_R, HH_CH); }
 
 void print() {
     for(int i = 0; i < CK_SIZE; ++i) {
         printf("%s\n", face[i]);
     }
+}
+
+// t in [0, CK_T)
+void draw_mhand(int t) { draw(hand[t], 1, MH_R, MH_CH); }
+void draw_hhand(int t) { draw(hand[t], 1, HH_R, HH_CH); }
+
+// h in [0, 12) and m in [0, 60)
+void draw_ckhands(int h, int m) {
+    draw_mhand(m);
+    draw_hhand((h % CK_T * 5 % CK_T) + (m / 12));
 }
 
 
@@ -60,8 +65,7 @@ int main() {
 
     time_t _now; time(&_now);
     struct tm *now = localtime(&_now);
-    draw_mhand(now->tm_min / 5);
-    draw_hhand(now->tm_hour % MXT);
+    draw_ckhands(now->tm_hour, now->tm_min);
 
     print();
 }
